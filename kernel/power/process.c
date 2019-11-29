db0f5b8956c5ac6717712ff1914e8026ef4eb7e8
Shilin Victor <chrono.monochrome@gmail.com>
2019-11-29 10:28:57 +0300

    Revert "kernel: power: Finish removing earlysuspend"
    This reverts commit 5d5992115d47fda122d8c1b036cb7018afd5e425.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -83,14 +83,11 @@ static int try_to_freeze_tasks(bool user_only)
 	do_div(elapsed_msecs64, NSEC_PER_MSEC);
 	elapsed_msecs = elapsed_msecs64;
 
-	if (wakeup) {
+	if (todo) {
 		printk("\n");
-		printk(KERN_ERR "Freezing of tasks aborted after %d.%03d seconds",
-		       elapsed_msecs / 1000, elapsed_msecs % 1000);
-	} else if (todo) {
-		printk("\n");
-		printk(KERN_ERR "Freezing of tasks failed after %d.%03d seconds"
-		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
+		printk(KERN_ERR "Freezing of tasks %s after %d.%03d seconds "
+		       "(%d tasks refusing to freeze, wq_busy=%d):\n",
+		       wakeup ? "aborted" : "failed",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
 		       todo - wq_busy, wq_busy);
 
