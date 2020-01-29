vova7878/gnu_linux_kernel_samsung_i9300

mms_ts: replace earlysuspend with fb notifier callbacks

drivers/input/touchscreen/mms_ts.c
@@ -28,7 +28,10 @@
 #define TSP_BUF_SIZE 1024
 #define FAIL -1
 #include <linux/delay.h>
-#include <linux/earlysuspend.h>
+#ifdef CONFIG_FB
+#include <linux/notifier.h>
+#include <linux/fb.h>
+#endif
 #include <linux/firmware.h>
 #include <linux/gpio.h>
 #include <linux/i2c.h>
@@ -245,7 +248,8 @@ struct mms_ts_info {
 	int (*power) (bool on);
 
 	struct melfas_tsi_platform_data *pdata;
-	struct early_suspend early_suspend;
+	struct notifier_block fb_notif;
+	bool fb_suspended;
 
 	/* protects the enabled flag */
 	struct mutex lock;
@@ -295,9 +299,11 @@ struct mms_fw_image {
 	u8 data[0];
 } __packed;
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static void mms_ts_early_suspend(struct early_suspend *h);
-static void mms_ts_late_resume(struct early_suspend *h);
+#ifdef CONFIG_FB
+static void mms_ts_fb_suspend(struct mms_ts_info *info);
+static void mms_ts_fb_resume(struct mms_ts_info *info);
+static int fb_notifier_callback(struct notifier_block *self,
+                                unsigned long event, void *data);
 #endif
 
 #if TOUCH_BOOSTER
@@ -3042,11 +3048,10 @@ static int __devinit mms_ts_probe(struct i2c_client *client,
 	if (info->register_cb)
 		info->register_cb(&info->callbacks);
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	info->early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1;
-	info->early_suspend.suspend = mms_ts_early_suspend;
-	info->early_suspend.resume = mms_ts_late_resume;
-	register_early_suspend(&info->early_suspend);
+#ifdef CONFIG_FB
+	info->fb_suspended = false;
+	info->fb_notif.notifier_call = fb_notifier_callback;
+	fb_register_client(&info->fb_notif);
 #endif
 
 	sec_touchscreen = device_create(sec_class,
@@ -3093,7 +3098,9 @@ static int __devexit mms_ts_remove(struct i2c_client *client)
 {
 	struct mms_ts_info *info = i2c_get_clientdata(client);
 
-	unregister_early_suspend(&info->early_suspend);
+#ifdef CONFIG_FB
+	unregister_fb_notifier(&info->fb_notif);
+#endif
 
 	if (info->irq >= 0)
 		free_irq(info->irq, info);
@@ -3103,7 +3110,7 @@ static int __devexit mms_ts_remove(struct i2c_client *client)
 	return 0;
 }
 
-#if defined(CONFIG_PM) || defined(CONFIG_HAS_EARLYSUSPEND)
+//#ifndef CONFIG_HAS_EARLYSUSPEND
 static int mms_ts_suspend(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -3157,26 +3164,56 @@ static int mms_ts_resume(struct device *dev)
 	mms_set_noise_mode(info);
 	return 0;
 }
-#endif
+//#endif
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static void mms_ts_early_suspend(struct early_suspend *h)
+#ifdef CONFIG_FB
+static void mms_ts_fb_suspend(struct mms_ts_info *info)
 {
-	struct mms_ts_info *info;
-	info = container_of(h, struct mms_ts_info, early_suspend);
+	if (info->fb_suspended)
+		return;
+
 	mms_ts_suspend(&info->client->dev);
-
+	info->fb_suspended = true;
 }
 
-static void mms_ts_late_resume(struct early_suspend *h)
+static void mms_ts_fb_resume(struct mms_ts_info *info)
 {
-	struct mms_ts_info *info;
-	info = container_of(h, struct mms_ts_info, early_suspend);
+	if (!info->fb_suspended)
+		return;
+
 	mms_ts_resume(&info->client->dev);
+	info->fb_suspended = false;
+}
+
+
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
+	struct mms_ts_info *mms = container_of(self, struct mms_ts_info, fb_notif);
+ 	if (evdata && evdata->data && mms) {
+		if (event == FB_EVENT_BLANK) {
+			blank = evdata->data;
+			switch (*blank) {
+				case FB_BLANK_UNBLANK:
+				case FB_BLANK_NORMAL:
+				case FB_BLANK_VSYNC_SUSPEND:
+				case FB_BLANK_HSYNC_SUSPEND:
+					mms_ts_fb_resume(mms);
+					break;
+				default:
+				case FB_BLANK_POWERDOWN:
+					mms_ts_fb_suspend(mms);
+					break;
+			}
+		}
+	}
+ 	return 0;
 }
 #endif
 
-#if defined(CONFIG_PM) && !defined(CONFIG_HAS_EARLYSUSPEND)
+#if defined(CONFIG_PM) && !defined(CONFIG_FB)
 static const struct dev_pm_ops mms_ts_pm_ops = {
 	.suspend = mms_ts_suspend,
 	.resume = mms_ts_resume,
@@ -3200,7 +3237,7 @@ static struct i2c_driver mms_ts_driver = {
 	.remove = __devexit_p(mms_ts_remove),
 	.driver = {
 		   .name = MELFAS_TS_NAME,
-#if defined(CONFIG_PM) && !defined(CONFIG_HAS_EARLYSUSPEND)
+#if defined(CONFIG_PM) && !defined(CONFIG_FB)
 		   .pm = &mms_ts_pm_ops,
 #endif
 		   },

leewp14/android_kernel_samsung_smdk4412-1

drivers/input/touchscreen/mms_ts.c
@@ -28,7 +28,10 @@
 #define TSP_BUF_SIZE 1024
 #define FAIL -1
 #include <linux/delay.h>
-#include <linux/earlysuspend.h>
+#ifdef CONFIG_FB
+#include <linux/notifier.h>
+#include <linux/fb.h>
+#endif
 #include <linux/firmware.h>
 #include <linux/gpio.h>
 #include <linux/i2c.h>
@@ -245,7 +248,8 @@ struct mms_ts_info {
 	int (*power) (bool on);
 
 	struct melfas_tsi_platform_data *pdata;
-	struct early_suspend early_suspend;
+	struct notifier_block fb_notif;
+	bool fb_suspended;
 
 	/* protects the enabled flag */
 	struct mutex lock;
@@ -295,9 +299,11 @@ struct mms_fw_image {
 	u8 data[0];
 } __packed;
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static void mms_ts_early_suspend(struct early_suspend *h);
-static void mms_ts_late_resume(struct early_suspend *h);
+#ifdef CONFIG_FB
+static void mms_ts_fb_suspend(struct mms_ts_info *info);
+static void mms_ts_fb_resume(struct mms_ts_info *info);
+static int fb_notifier_callback(struct notifier_block *self,
+                                unsigned long event, void *data);
 #endif
 
 #if TOUCH_BOOSTER
@@ -3042,11 +3048,10 @@ static int __devinit mms_ts_probe(struct i2c_client *client,
 	if (info->register_cb)
 		info->register_cb(&info->callbacks);
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	info->early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1;
-	info->early_suspend.suspend = mms_ts_early_suspend;
-	info->early_suspend.resume = mms_ts_late_resume;
-	register_early_suspend(&info->early_suspend);
+#ifdef CONFIG_FB
+	info->fb_suspended = false;
+	info->fb_notif.notifier_call = fb_notifier_callback;
+	fb_register_client(&info->fb_notif);
 #endif
 
 	sec_touchscreen = device_create(sec_class,
@@ -3093,7 +3098,9 @@ static int __devexit mms_ts_remove(struct i2c_client *client)
 {
 	struct mms_ts_info *info = i2c_get_clientdata(client);
 
-	unregister_early_suspend(&info->early_suspend);
+#ifdef CONFIG_FB
+	fb_unregister_client(&info->fb_notif);
+#endif
 
 	if (info->irq >= 0)
 		free_irq(info->irq, info);
@@ -3103,7 +3110,7 @@ static int __devexit mms_ts_remove(struct i2c_client *client)
 	return 0;
 }
 
-#if defined(CONFIG_PM) || defined(CONFIG_HAS_EARLYSUSPEND)
+//#ifndef CONFIG_HAS_EARLYSUSPEND
 static int mms_ts_suspend(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -3157,26 +3164,56 @@ static int mms_ts_resume(struct device *dev)
 	mms_set_noise_mode(info);
 	return 0;
 }
-#endif
+//#endif
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static void mms_ts_early_suspend(struct early_suspend *h)
+#ifdef CONFIG_FB
+static void mms_ts_fb_suspend(struct mms_ts_info *info)
 {
-	struct mms_ts_info *info;
-	info = container_of(h, struct mms_ts_info, early_suspend);
+	if (info->fb_suspended)
+		return;
+
 	mms_ts_suspend(&info->client->dev);
-
+	info->fb_suspended = true;
 }
 
-static void mms_ts_late_resume(struct early_suspend *h)
+static void mms_ts_fb_resume(struct mms_ts_info *info)
 {
-	struct mms_ts_info *info;
-	info = container_of(h, struct mms_ts_info, early_suspend);
+	if (!info->fb_suspended)
+		return;
+
 	mms_ts_resume(&info->client->dev);
+	info->fb_suspended = false;
+}
+
+
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
+	struct mms_ts_info *mms = container_of(self, struct mms_ts_info, fb_notif);
+ 	if (evdata && evdata->data && mms) {
+		if (event == FB_EVENT_BLANK) {
+			blank = evdata->data;
+			switch (*blank) {
+				case FB_BLANK_UNBLANK:
+				case FB_BLANK_NORMAL:
+				case FB_BLANK_VSYNC_SUSPEND:
+				case FB_BLANK_HSYNC_SUSPEND:
+					mms_ts_fb_resume(mms);
+					break;
+				default:
+				case FB_BLANK_POWERDOWN:
+					mms_ts_fb_suspend(mms);
+					break;
+			}
+		}
+	}
+ 	return 0;
 }
 #endif
 
-#if defined(CONFIG_PM) && !defined(CONFIG_HAS_EARLYSUSPEND)
+#if defined(CONFIG_PM) && !defined(CONFIG_FB)
 static const struct dev_pm_ops mms_ts_pm_ops = {
 	.suspend = mms_ts_suspend,
 	.resume = mms_ts_resume,
@@ -3200,7 +3237,7 @@ static struct i2c_driver mms_ts_driver = {
 	.remove = __devexit_p(mms_ts_remove),
 	.driver = {
 		   .name = MELFAS_TS_NAME,
-#if defined(CONFIG_PM) && !defined(CONFIG_HAS_EARLYSUSPEND)
+#if defined(CONFIG_PM) && !defined(CONFIG_FB)
 		   .pm = &mms_ts_pm_ops,
 #endif
 		   },