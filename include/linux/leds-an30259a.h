zhafknight/android_kernel_samsung_smdk4412

samsung_battery, leds-an30259a: add charge level led notification

include/linux/leds-an30259a.h
@@ -80,4 +80,19 @@ struct an30259a_pr_control {
 #define AN30259A_PR_SET_LED	_IOW('S', 42, struct an30259a_pr_control)
 #define AN30259A_PR_SET_LEDS	_IOW('S', 43, struct an30259a_pr_control[3])
 #define AN30259A_PR_SET_IMAX	_IOW('S', 44, __u8)
+
+#define COLOR_RED	0xff0000
+#define COLOR_GREEN	0x00ff00
+#define COLOR_BLUE	0x0000ff
+#define COLOR_YELLOW	COLOR_RED | COLOR_GREEN
+
+void enable_led_an30259a(unsigned int led_brightness, unsigned int delay_on_time,
+							unsigned int delay_off_time);
+
+extern unsigned int led_switch;
+extern unsigned int color_fullcharge;
+extern unsigned int color_highcharge;
+extern unsigned int color_medcharge;
+extern unsigned int color_lowcharge;
+
 #endif						/* _LEDS_AN30259A_H */

leewp14/android_kernel_samsung_smdk4412-1

Change-Id: I0445dbf6c50bf97386b5112432483fdf0c45fa98

include/linux/leds-an30259a.h
@@ -80,4 +80,19 @@ struct an30259a_pr_control {
 #define AN30259A_PR_SET_LED	_IOW('S', 42, struct an30259a_pr_control)
 #define AN30259A_PR_SET_LEDS	_IOW('S', 43, struct an30259a_pr_control[3])
 #define AN30259A_PR_SET_IMAX	_IOW('S', 44, __u8)
+
+#define COLOR_RED	0x0d0000
+#define COLOR_GREEN	0x000d00
+#define COLOR_BLUE	0x00000d
+#define COLOR_YELLOW	COLOR_RED | COLOR_GREEN
+
+void enable_led_an30259a(unsigned int led_brightness, unsigned int delay_on_time,
+							unsigned int delay_off_time);
+
+extern unsigned int led_switch;
+extern unsigned int color_fullcharge;
+extern unsigned int color_highcharge;
+extern unsigned int color_medcharge;
+extern unsigned int color_lowcharge;
+
 #endif						/* _LEDS_AN30259A_H */