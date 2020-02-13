vova7878/gnu_linux_kernel_samsung_i9300

sensorhub: switch from wakelocks to wakeup sources
Change-Id: I3b6057bc3c6fed3dab9e24712c82769b23055d33

drivers/sensorhub/atmel/ssp_sensorhub.c
@@ -212,7 +212,7 @@ static void ssp_sensorhub_report_library(struct ssp_sensorhub_data *hub_data)
 {
 	input_report_rel(hub_data->sensorhub_input_dev, DATA, DATA);
 	input_sync(hub_data->sensorhub_input_dev);
-	wake_lock_timeout(&hub_data->sensorhub_wake_lock, WAKE_LOCK_TIMEOUT);
+	__pm_wakeup_event(&hub_data->sensorhub_wake_lock, WAKE_LOCK_TIMEOUT);
 }
 
 static void ssp_sensorhub_report_large_library(
@@ -220,7 +220,7 @@ static void ssp_sensorhub_report_large_library(
 {
 	input_report_rel(hub_data->sensorhub_input_dev, LARGE_DATA, LARGE_DATA);
 	input_sync(hub_data->sensorhub_input_dev);
-	wake_lock_timeout(&hub_data->sensorhub_wake_lock, WAKE_LOCK_TIMEOUT);
+	__pm_wakeup_event(&hub_data->sensorhub_wake_lock, WAKE_LOCK_TIMEOUT);
 }
 
 static int ssp_sensorhub_list(struct ssp_sensorhub_data *hub_data,
@@ -481,8 +481,7 @@ int ssp_sensorhub_initialize(struct ssp_data *ssp_data)
 	ssp_data->hub_data = hub_data;
 
 	/* init wakelock, list, waitqueue, completion and spinlock */
-	wake_lock_init(&hub_data->sensorhub_wake_lock, WAKE_LOCK_SUSPEND,
-			"ssp_sensorhub_wake_lock");
+	wakeup_source_init(&hub_data->sensorhub_wake_lock, "ssp_sensorhub_wake_lock");
 	INIT_LIST_HEAD(&hub_data->events_head.list);
 	init_waitqueue_head(&hub_data->sensorhub_wq);
 	init_completion(&hub_data->sensorhub_completion);
@@ -539,7 +538,7 @@ err_misc_register:
 err_input_register_device_sensorhub:
 err_input_allocate_device_sensorhub:
 	complete_all(&hub_data->sensorhub_completion);
-	wake_lock_destroy(&hub_data->sensorhub_wake_lock);
+	wakeup_source_trash(&hub_data->sensorhub_wake_lock);
 	kfree(hub_data);
 exit:
 	return ret;
@@ -557,7 +556,7 @@ void ssp_sensorhub_remove(struct ssp_data *ssp_data)
 	misc_deregister(&hub_data->sensorhub_device);
 	input_unregister_device(hub_data->sensorhub_input_dev);
 	complete_all(&hub_data->sensorhub_completion);
-	wake_lock_destroy(&hub_data->sensorhub_wake_lock);
+	wakeup_source_trash(&hub_data->sensorhub_wake_lock);
 	kfree(hub_data);
 }
 

leewp14/android_kernel_samsung_smdk4412-1

drivers/sensorhub/ssp_sensorhub.c
@@ -406,7 +406,7 @@ static int ssp_senosrhub_thread_func(void *arg)
 			/* report sensorhub event to user */
 			ssp_report_sensorhub_length(hub_data,
 					hub_data->first_event->length);
-			wake_lock_timeout(&hub_data->sensorhub_wake_lock, 5*HZ);
+			__pm_wakeup_event(&hub_data->sensorhub_wake_lock, 5000);
 			hub_data->transfer_ready++;
 		}
 
@@ -472,7 +472,7 @@ int ssp_handle_sensorhub_large_data(struct ssp_data *ssp_data, u8 sub_cmd)
 	if (ret >= 0) {
 		ssp_report_sensorhub_length(hub_data,
 			hub_data->large_library_length);
-		wake_lock_timeout(&hub_data->sensorhub_wake_lock, 3*HZ);
+		__pm_wakeup_event(&hub_data->sensorhub_wake_lock, 3000);
 	} else {
 		pr_err("%s: ssp_receive_large_msg err(%d)", __func__, ret);
 	}
@@ -502,8 +502,7 @@ int ssp_initialize_sensorhub(struct ssp_data *ssp_data)
 		goto err_input_allocate_device_sensorhub;
 	}
 
-	wake_lock_init(&hub_data->sensorhub_wake_lock, WAKE_LOCK_SUSPEND,
-			"sensorhub_wake_lock");
+	wakeup_source_init(&hub_data->sensorhub_wake_lock, "sensorhub_wake_lock");
 	INIT_LIST_HEAD(&hub_data->events_head.list);
 	init_waitqueue_head(&hub_data->sensorhub_waitqueue);
 	init_completion(&hub_data->transfer_done);
@@ -548,7 +547,7 @@ err_misc_register:
 	input_unregister_device(hub_data->sensorhub_input_dev);
 err_input_register_device_sensorhub:
 	complete_all(&hub_data->transfer_done);
-	wake_lock_destroy(&hub_data->sensorhub_wake_lock);
+	wakeup_source_trash(&hub_data->sensorhub_wake_lock);
 err_input_allocate_device_sensorhub:
 	kfree(hub_data);
 	return ret;
@@ -562,7 +561,7 @@ void ssp_remove_sensorhub(struct ssp_data *ssp_data)
 	ssp_sensorhub_fops.unlocked_ioctl = NULL;
 	misc_deregister(&hub_data->sensorhub_device);
 	input_unregister_device(hub_data->sensorhub_input_dev);
-	wake_lock_destroy(&hub_data->sensorhub_wake_lock);
+	wakeup_source_trash(&hub_data->sensorhub_wake_lock);
 	complete_all(&hub_data->transfer_done);
 	if (hub_data->sensorhub_task)
 		kthread_stop(hub_data->sensorhub_task);